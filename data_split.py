da85ae6d80787c8060400985ec1ce7bfac98fe08
Yoongon Noh <35756931+nyg520@users.noreply.github.com>
2023-07-09 09:49:47 +0900

    Fixed code error

--- data_split.py
+++ data_split.py
@@ -22,7 +22,7 @@ def main():
     rt_path = os.path.join(args.data_path, 'rt_img')
     flist = os.listdir(rt_path)
 
-    np.save(os.path.join(args.save_path, 'full_dataset.npy', flist))
+    np.save(os.path.join(args.save_path, 'full_dataset.npy'), flist)
 
     # Train-and-validation / Internal test dataset split
     train_val_list = []
